a0bf0bdb62851049cf48cab1c1ece561abc212a2
Kenta SUZUKI <suzuken326@gmail.com>
2016-08-04 14:41:40 +0900

    add editorconfig and eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    parser: 'babel-eslint',
+    env: {
+        es6: true,
+        browser: true
+    },
+    plugins: [
+        "react"
+    ],
+    ecmaFeatures: {
+        jsx: true,
+        modules: true
+    },
+    globals: {},
+    rules: {
+        'no-console': 0,
+        'react/no-danger': 0,
+        'react/prop-types': 1
+    }
+};
